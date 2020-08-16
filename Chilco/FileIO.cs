67e6ea86f04a66f52d42794c4dccd3bc25c7abd1
Unknown <pavel.kessler@gmx.de>
2020-08-16 14:10:50 +0200

    Created option for default settings file and fixed file loading error

--- Chilco/FileIO.cs
+++ Chilco/FileIO.cs
@@ -7,18 +7,36 @@ namespace Chilco
     {
        
         private static readonly string groupPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"settings.json");
-        
+        private static readonly string defaultGroupPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"default_settings.json");
+
         public static Group[] LoadGroups()
         {
-            if (File.Exists(groupPath))
-            {
-                string json = File.ReadAllText(groupPath);
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<Group[]>(json);
-            }
+            Group[] groups = null;
+
+            groups = Loadfile(groupPath);
+            if (groups != null) return groups;
+
+            groups = Loadfile(defaultGroupPath);
+            if (groups != null) return groups;
 
             return new Group[0];
         }
-
+        private static Group[] Loadfile(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    string json = File.ReadAllText(path);
+                    if (json != null && json != "")
+                    {
+                        return Newtonsoft.Json.JsonConvert.DeserializeObject<Group[]>(json);
+                    }
+                }
+                return null;
+            }
+            catch (Exception) { return null; }
+        }
         public static void SaveGroups(Group[] groups)
         {
             File.Create(groupPath).Close();
